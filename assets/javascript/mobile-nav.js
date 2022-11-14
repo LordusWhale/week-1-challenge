cd0a6231a9d3d6c292cae3407b6d014664cc6213
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-14 11:13:32 +1100

    Added SVG menu button

--- assets/javascript/mobile-nav.js
+++ assets/javascript/mobile-nav.js
@@ -1,17 +1,19 @@
 const mobileNav = document.getElementById("mobile-nav");
 const mobileMenuContent = document.getElementById("mobile-menu-content");
 const toggleNavButton = document.getElementById("toggle-nav");
-
+const openMenuSVG = document.getElementById('open-menu');
 let toggleNav = false;
 
 const closeMenu = () => {
   mobileNav.style.left = "-100%";
   mobileMenuContent.style.left = "-100%";
+  openMenuSVG.setAttribute('width', 100);
 };
 
 const openMenu = () => {
   mobileNav.style.left = 0;
   mobileMenuContent.style.left = 0;
+  openMenuSVG.setAttribute('width', 80);
 };
 
 toggleNavButton.addEventListener("click", (e) => {
